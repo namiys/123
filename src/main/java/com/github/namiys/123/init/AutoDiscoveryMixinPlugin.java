77d429a88d252df19b79ea7d56488065404b15c9
GitHub Action <action@github.com>
2025-04-03 06:24:02 +0000

    Initialize template repository proper
    Set package name to com.github.namiys.123
    Set project name to 123
    Set modid to

--- src/main/java/com/github/namiys/123/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/namiys/123/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.namiys.123.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
